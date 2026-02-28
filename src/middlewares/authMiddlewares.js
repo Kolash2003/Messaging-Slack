8c510bd873dd8df47e20c5a9afb6296898e8f4d4
Aneesh Kolar <kolaraneesh2003@gmail.com>
2026-02-28 23:15:14 +0530

    feat: implement JWT authentication middleware and create initial Mongoose schemas for channel, message, and workspace.

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,42 @@
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+
+import { JWT_SECRET } from "../config/serverConfig";
+import userRepository from "../repositories/userRepository";
+import { customErrorResponse, internalErrorResponse } from "../utils/common/responseObjects";
+
+export const isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.headers['x-access-token'];
+        if (!token) {
+            return res.status(StatusCodes.FORBIDDEN).json(customErrorResponse({
+                explanation: ['No token provided'],
+                message: 'No auth token provided',
+                statusCode: StatusCodes.FORBIDDEN
+            }))
+        }
+        const response = jwt.verify(token, JWT_SECRET);
+
+        if (!response) {
+            return res.status(StatusCodes.FORBIDDEN).json(customErrorResponse({
+                explanation: ['Invalid data sent from the client'],
+                message: 'Invalid auth token provided',
+                statusCode: StatusCodes.FORBIDDEN
+            }));
+        }
+
+        const user = await userRepository.getById(response.id);
+        req.user = user.id;
+        next();
+    } catch (error) {
+        console.log('Auth middleware error', error);
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(StatusCodes.FORBIDDEN).json(customErrorResponse({
+                explanation: ['Invalid data sent from the client'],
+                message: 'Invalid auth token provided',
+                statusCode: StatusCodes.FORBIDDEN
+            }));
+        }
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(internalErrorResponse(error));
+    }
+}
